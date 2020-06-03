fc9ed63a7c16a5d0b1d7ef15b4f14fbaadaf884d
Luc De Meyer <luc.demeyer@noviat.com>
2020-06-03 09:14:32 +0200

    [12.0][IMP]coda processing

---
+++ l10n_be_coda_card_cost/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2009-2020 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'CODA Import - Handle Payment Card Cost',
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Noviat',
+    'website': 'http://www.noviat.com',
+    'category': 'Accounting & Finance',
+    'complexity': 'normal',
+    'summary': 'CODA Import - Handle Payment Card Cost',
+    'depends': [
+        'l10n_be_coda_advanced',
+    ],
+    'data': [
+        'views/coda_bank_account_views.xml',
+    ],
+    'installable': True,
+}
