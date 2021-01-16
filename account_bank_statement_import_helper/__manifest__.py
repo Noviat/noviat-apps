7ff68438db5f2a79d2cdcfe4b5beae00f650ba78
Luc De Meyer <luc.demeyer@noviat.com>
2021-01-16 18:52:08 +0100

    [13.0][IMP]remove currency from local bank accounts to find journal

--- account_bank_statement_import_helper/__manifest__.py
+++ account_bank_statement_import_helper/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2009-2020 Noviat.
+# Copyright 2009-2021 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Bank statement import helper",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "category": "Accounting & Finance",
     "summary": """
         Generic bank statement import improvements.
--- account_bank_statement_import_helper/wizard/account_bank_statement_import.py
+++ account_bank_statement_import_helper/wizard/account_bank_statement_import.py
@@ -1,4 +1,4 @@
-# Copyright 2009-2020 Noviat.
+# Copyright 2009-2021 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models
@@ -8,8 +8,9 @@ from odoo.addons.base.models.res_bank import sanitize_account_number
 
 class AccountBankStatementImport(models.TransientModel):
     """
-    add support for local bank account numbers which are in several
-    countries a subset of the IBAN
+    Add support for local bank account numbers which are in several
+    countries a subset of the IBAN.
+    Some banks also add the currency at the end of the local account number.
     """
 
     _inherit = "account.bank.statement.import"
@@ -17,11 +18,11 @@ class AccountBankStatementImport(models.TransientModel):
     def _find_additional_data(self, currency_code, account_number):
         currency, journal = super()._find_additional_data(currency_code, account_number)
         if not journal:
-            sanitized_account_number = sanitize_account_number(account_number)
+            sanitized_account_number = self._sanitize_account_number(account_number)
             fin_journals = self.env["account.journal"].search([("type", "=", "bank")])
             fin_journal = fin_journals.filtered(
                 lambda r: sanitized_account_number
-                in (r.bank_account_id.sanitized_acc_number or '')
+                in (r.bank_account_id.sanitized_acc_number or "")
             )
             if len(fin_journal) == 1:
                 journal = fin_journal
@@ -30,5 +31,15 @@ class AccountBankStatementImport(models.TransientModel):
     def _check_journal_bank_account(self, journal, account_number):
         check = super()._check_journal_bank_account(journal, account_number)
         if not check:
-            check = account_number in journal.bank_account_id.sanitized_acc_number
+            number = self._sanitize_account_number(account_number)
+            check = number in journal.bank_account_id.sanitized_acc_number
         return check
+
+    def _sanitize_account_number(self, account_number):
+        sanitized_number = sanitize_account_number(account_number)
+        check_curr = sanitized_number[-3:]
+        if check_curr.isalpha():
+            all_currencies = self.env["res.currency"].search([])
+            if check_curr in all_currencies.mapped("name"):
+                sanitized_number = sanitized_number[:-3]
+        return sanitized_number
