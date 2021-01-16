744592338baa8a9337d55ac92d42e836a27ba8dc
Luc De Meyer <luc.demeyer@noviat.com>
2021-01-16 12:28:37 +0100

    fix account_partner_open_items_xlsx

--- account_partner_open_items_xlsx/__manifest__.py
+++ account_partner_open_items_xlsx/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2009-2020 Noviat.
+# Copyright 2009-2021 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Open Journal Items per partner',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'license': 'AGPL-3',
     'author': 'Noviat',
     'website': 'http://www.noviat.com',
