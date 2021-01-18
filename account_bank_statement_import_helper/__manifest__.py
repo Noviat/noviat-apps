1fdc4af15d33f49a51465222ff458924354ab31d
Luc De Meyer <luc.demeyer@noviat.com>
2021-01-18 19:14:16 +0100

    remove OCA from manifest & readme since PR didn't get merged

--- account_bank_statement_import_helper/__manifest__.py
+++ account_bank_statement_import_helper/__manifest__.py
@@ -5,11 +5,8 @@
     "name": "Bank statement import helper",
     "version": "13.0.1.0.1",
     "category": "Accounting & Finance",
-    "summary": """
-        Generic bank statement import improvements.
-    """,
-    "author": "Noviat, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/bank-statement-import",
+    "author": "Noviat",
+    "website": "www.noviat.com",
     "depends": ["account_bank_statement_import"],
     "installable": True,
     "license": "AGPL-3",
