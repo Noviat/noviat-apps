c5d8d1187aea802c363437b4256e59a4d0c1ddfd
Luc De Meyer <luc.demeyer@noviat.com>
2021-02-25 09:30:52 +0100

    fix nbb reports + update tax templates

--- l10n_be_coa_multilang/__manifest__.py
+++ l10n_be_coa_multilang/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2009-2020 Noviat
+# Copyright 2009-2021 Noviat
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Belgium - Multilingual Chart of Accounts (en/nl/fr)",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.2",
     "license": "AGPL-3",
     "author": "Noviat",
     "website": "http://www.noviat.com",
