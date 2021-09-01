62eb094429b96ca9c4c9f83edf5e20b906480342
Luc De Meyer <luc.demeyer@noviat.com>
2021-09-01 13:49:55 +0200

    [14.0]add statement_import_helper modules

---
+++ account_bank_statement_import_helper/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2009-2021 Noviat.
+# License LGPL-3 or later (http://www.gnu.org/licenses/lpgl).
+
+{
+    "name": "Bank statement import helper",
+    "version": "14.0.1.0.0",
+    "category": "Accounting & Finance",
+    "author": "Noviat",
+    "website": "www.noviat.com",
+    "depends": ["account_bank_statement_import"],
+    "installable": True,
+    "license": "LGPL-3",
+}
