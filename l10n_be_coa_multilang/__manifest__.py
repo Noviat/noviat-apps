861f89a9577157d5378cd9b4dc074dff92507f4e
Luc De Meyer <luc.demeyer@noviat.com>
2022-11-08 20:34:47 +0100

    [13.0]fix tax template for 06% EU purchase tax

--- l10n_be_coa_multilang/__manifest__.py
+++ l10n_be_coa_multilang/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2009-2021 Noviat
+# Copyright 2009-2022 Noviat
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Belgium - Multilingual Chart of Accounts (en/nl/fr)",
-    "version": "13.0.1.1.1",
+    "version": "13.0.1.1.2",
     "license": "AGPL-3",
     "author": "Noviat",
     "website": "http://www.noviat.com",
