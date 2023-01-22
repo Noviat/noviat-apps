f2f7c12c8ed44f3fc190edf036daebc2aa32ac58
Luc De Meyer <luc.demeyer@noviat.com>
2023-01-22 11:10:22 +0100

    [14.0]update l10n_be_coa_multilang

---
+++ l10n_be_coa_multilang_oe/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2009-2022 Noviat
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Belgium - Multilingual Chart of Accounts (en/nl/fr) on Odoo Enterprise",
+    "version": "14.0.1.0.0",
+    "category": "Hidden",
+    "license": "LGPL-3",
+    "author": "Noviat",
+    "website": "https://www.noviat.com/",
+    "summary": "Belgium - Multilingual Chart of Accounts (en/nl/fr) on Odoo Enterprise",
+    "depends": ["l10n_be_coa_multilang", "account_reports"],
+    "installable": True,
+    "auto_install": True,
+}
