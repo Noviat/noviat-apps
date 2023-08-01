ce00eb3b92093451a789daa002b3dff0be48bf73
Luc De Meyer <luc.demeyer@noviat.com>
2023-08-01 10:16:15 +0200

    add support for monetary & selection fields

--- account_asset_management_import/__manifest__.py
+++ account_asset_management_import/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2009-2022 Noviat.
+# Copyright 2009-2023 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Fixed Assets import",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "license": "AGPL-3",
     "author": "Noviat",
     "website": "https://www.noviat.com/",
