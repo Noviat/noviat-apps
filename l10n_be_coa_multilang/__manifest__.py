ca42c4750ab622c76cea29e8a85ae0b9fb240bf3
Luc De Meyer <luc.demeyer@noviat.com>
2024-02-27 21:44:22 +0100

    [FIX]l10n_be_coa_multilang - fix VAT listing
    The annual client listing does not include customers with amount < 250
    hence customers with only Credit Notes did not appear. This is now
    fixed by checking absolute amount.

--- l10n_be_coa_multilang/__manifest__.py
+++ l10n_be_coa_multilang/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2009-2023 Noviat
+# Copyright 2009-2024 Noviat
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Belgium - Multilingual Chart of Accounts (en/nl/fr)",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "license": "AGPL-3",
     "author": "Noviat",
     "website": "https://www.noviat.com/",
--- l10n_be_coa_multilang/models/account_report_line.py
+++ l10n_be_coa_multilang/models/account_report_line.py
@@ -7,3 +7,12 @@ from odoo import models
 class AccountReportLine(models.Model):
     _name = "account.report.line"
     _inherit = ["account.report.line", "l10n.be.chart.common"]
+
+    _sql_constraints = [
+        # this constraint has been adopted by Odoo in 17.0
+        (
+            "code_uniq",
+            "unique (code, report_id)",
+            "A report line with the same code already exists.",
+        ),
+    ]
